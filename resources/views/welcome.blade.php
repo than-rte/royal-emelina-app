0efd56337567029a61d0ec5ab28a156abe566698
than-rte <me.nathanran@gmail.com>
2020-10-16 06:16:23 +0800

    change home title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GOOD MORNING M*THER F*CKERS
                 </div>
 
                 <div class="links">
